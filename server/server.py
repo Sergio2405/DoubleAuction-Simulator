874950fe9370cd4142c67af7a81f33ba41d60050
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-10-12 20:40:27 -0500

    "fastapi websockets route added"

--- server/server.py
+++ server/server.py
@@ -15,18 +15,11 @@ app = FastAPI()
 async def connected():
     return "Hello from the server"
 
-@app.websocket("/ws")
-async def websocket_server(websocket:WebSocket):
+@app.websocket("/market_socket")
+async def market_server(websocket:WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.end_text(f"Message has been received: {data}")
-
-async def exchange(websocket):
-    
-    async for message in websocket:
-
-        message = json.loads(message)
+        message = await websocket.receive_json()
         
         if message["setup"]: 
             market = Market()
@@ -34,7 +27,7 @@ async def exchange(websocket):
             market.setupMarket(duration = setup["duration"])
             response = {"log": "Market is open", "time": datetime.now().strftime('%H:%M:%S.%f')}
             print("Market has opened!")
-            await websocket.send(json.dumps(response)) 
+            await websocket.send_json(response) 
         else:
             if datetime.now() < market.duration: 
                 order = message
@@ -44,17 +37,19 @@ async def exchange(websocket):
                     response = market.addOrder(order)
 
                 response["time"] = datetime.now().strftime('%H:%M:%S.%f')
-                await websocket.send(json.dumps(response))
+                await websocket.send_json(response)
             else:
                 response = {"log" : "Market is closed", "time" : datetime.now().strftime('%H:%M:%S.%f')}
-                await websocket.send(json.dumps(response))
+                await websocket.send_json(response)
                 print("Market is Closed!")
                 await websocket.close()
-        
-async def market():
-    print("[SERVER UP]")
-    async with websockets.serve(exchange, HOST, PORT):
-        await asyncio.Future() 
+       
+        await websocket.send_json({"log":"Message has been received"})
+
+#async def market():
+#    print("[SERVER UP]")
+#    async with websockets.serve(exchange, HOST, PORT):
+#        await asyncio.Future() 
 
 if __name__ == "__main__":
     asyncio.run(market())
